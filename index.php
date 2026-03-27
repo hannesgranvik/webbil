15a54743ea099de54abc072af89e576dbc42246d
Sebastian Nyberg <sebastiannyberg@axxell.fi>
2026-03-27 09:41:13 +0200

    frontpage, functions and dipadapa

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <?php
 require_once "includes/config.php";
 require_once "includes/header.php";
+require_once "includes/functions.php";
+
 
 ?>
 
-<div class="container-mt-4">
-    <h1>Företagsannonser</h1>
-    <p>Någo någon nånstans</p>
+<?php
+$fetchannonser = fetchAnnons($pdo);
+foreach($fetchannonser as $annons) :
+?>
+
+
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="assets/ford1901something.jpg" alt="">
+  <div class="card-body">
+    <h5 class="card-title"><?php echo ($annons['marke']) . " " . ($annons['modell'])?></h5>
+    <p class="card-text"><?php echo $annons['beskrivning'] ?></p>
+  </div>
 </div>
 
 <?php
+	endforeach;
+
 require_once "includes/footer.php";
 ?>
